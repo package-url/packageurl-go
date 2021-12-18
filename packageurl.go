fdd45fab1ae3d53240e6f8b7d5a62a42f6a30296
Christian Rebischke <chris@shibumi.dev>
2021-12-18 15:21:49 +0100

    feat: add cargo custom type

--- packageurl.go
+++ packageurl.go
@@ -47,6 +47,8 @@ var (
 var (
 	// TypeBitbucket is a pkg:bitbucket purl.
 	TypeBitbucket = "bitbucket"
+	// TypeCargo is a pkg:cargo purl.
+	TypeCargo = "cargo"
 	// TypeCran is a pkg:cran purl.
 	TypeCran = "cran"
 	// TypeConan is pkg:conan purl.
